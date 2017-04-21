eb4e86393130bcc87f825ef6deeae1e4c2b3c0bc
Daniel Bizup <danbiz1@vt.edu>
2017-04-20 20:40:00 -0400

    Rover now sends out its correct position every 6 seconds after initial orientation.

--- src/navigation.c
+++ src/navigation.c
@@ -748,6 +748,10 @@ void NAVIGATION_Tasks ( void )
                     commMsg[COMM_CHECKSUM_IDX] = commCalculateChecksum(commMsg);
                     commSendMsg(commMsg);
                     pathfindingCount = 0;
+                    
+                    //Update position
+                    sprintf(commMsg,"*{\"S\":\"f\",\"T\":\"a\",\"M\":\"p\",\"N\":1,\"F\":[1,12,6,6,%d,%d,0],\"C\":1}~", (int) (GetLocationX() / 2), (int) (GetLocationY() / 2));
+                    commSendMsgToWifiQueue(commMsg);
                 }
                 
                 //Ignore tape for x seconds
